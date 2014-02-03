5ab9d0a2cf256d9d2b1510c99a07d6f1ffd6d6ab
pekepeke <pekepekesamurai@gmail.com>
2014-02-03 11:52:27 +0900

    add dolipo

---
+++ Casks/dolipo.rb
@@ -0,0 +1,7 @@
+class Dolipo < Cask
+  url 'http://dolipo.googlecode.com/files/dolipo.zip'
+  homepage 'http://drikin.com/'
+  version '1.0.8.3'
+  sha1 '14438608dce5dd658621bbaaf8f0c61717f74090'
+  link 'dolipo.app'
+end
