b0ec83b1dccda2eb22654988cef749c8b44fec16
pekepeke <pekepekesamurai@gmail.com>
2014-02-03 12:05:14 +0900

    update dolipo binary

--- Casks/dolipo.rb
+++ Casks/dolipo.rb
@@ -1,7 +1,9 @@
 class Dolipo < Cask
-  url 'http://dolipo.googlecode.com/files/dolipo.zip'
   homepage 'http://drikin.com/'
   version '1.0.8.3'
-  sha1 '14438608dce5dd658621bbaaf8f0c61717f74090'
+  # url 'http://dolipo.googlecode.com/files/dolipo.zip'
+  # sha1 '14438608dce5dd658621bbaaf8f0c61717f74090'
+  url 'http://dl.dropbox.com/u/1025/misc/dolipo.zip'
+  sha1 '6df8332f534a0ca8c2a1111cd5223afce666c8a6'
   link 'dolipo.app'
 end
