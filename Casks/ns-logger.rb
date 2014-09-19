6c1186180f0a134a69565327b75f6985af4a0e92
pekepeke <pekepekesamurai+github@gmail.com>
2014-09-19 19:29:48 +0900

    fixes sha256

--- Casks/ns-logger.rb
+++ Casks/ns-logger.rb
@@ -1,6 +1,6 @@
 class NsLogger < Cask
   version '1.5'
-  sha256 'af1611e66697bc8476884ec7bdcf1e5b6ffa2703f87b34b0c7d759727f55b960'
+  sha256 'f0ecb94187ab6b46e6482541d707265e4c553c7a47c0522cf647ee3f1c06637c'
 
   url 'https://github.com/fpillet/NSLogger/releases/download/v1.5/NSLogger-Desktop.Viewer.1.5.zip'
   homepage 'https://github.com/fpillet/NSLogger'
