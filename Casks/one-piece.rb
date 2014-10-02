3626339cdee133477934e2e2adc63b9b696c1165
pekepeke <pekepekesamurai+github@gmail.com>
2014-10-02 21:18:51 +0900

    add 1Piece.app

---
+++ Casks/one-piece.rb
@@ -0,0 +1,8 @@
+class OnePiece < Cask
+  url 'http://www001.upp.so-net.ne.jp/app1piece/1Piece-0.3.0.zip'
+  homepage 'http://www001.upp.so-net.ne.jp/app1piece/'
+  version '0.3.0'
+  sha256 '2622c4cf42e8852411f173843523914586342c25c8185431bb2025b398c54b6f'
+  # no_checksum
+  link '1Piece.app'
+end
