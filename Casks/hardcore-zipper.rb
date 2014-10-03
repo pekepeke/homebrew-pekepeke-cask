9e192d5e908c0807727fe04fc1634bc1e76d3788
pekepeke <pekepekesamurai+github@gmail.com>
2014-10-03 22:22:24 +0900

    fixes sha256

--- Casks/hardcore-zipper.rb
+++ Casks/hardcore-zipper.rb
@@ -2,7 +2,8 @@ class HardcoreZipper < Cask
   url 'http://www.geocities.jp/freeggggroup/nf/HardCoreZipper_213.zip'
   homepage 'http://www.geocities.jp/freeggggroup/nf/'
   version '2.1.3'
-  sha1 'dbc3ec33fa85a7bed9452757c0bf6ac0d6d22e7e'
+  sha256 '745e0b9775fc2416981b44a7e6c3ba72cc6b2692ab3fa843d980373fc921d081'
   link 'HardCoreZipper.app'
+  # TODO : extract
 end
 
--- Casks/stroy.rb
+++ Casks/stroy.rb
@@ -2,6 +2,6 @@ class Stroy < Cask
   url 'http://stroy.wikidot.com/local--files/download/stroy-app-intel-1.0.3.zip'
   homepage 'http://stroy.wikidot.com/start'
   version '1.0.3'
-  sha1 '09254279f81b1bebb5751b2e60f1c2d708fdf46e'
+  sha256 'af355f4409c1a5b83181f02dc22008c41123a06414197ae7acb1858cb02b9b9f'
   link 'stroy.app'
 end
