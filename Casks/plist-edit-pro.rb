7daa36efad80de8a6b7520096fa18e1a21e1b379
pekepeke <pekepekesamurai+github@gmail.com>
2014-10-03 14:15:10 +0900

    add PListEdit Pro, SimPholders2

---
+++ Casks/plist-edit-pro.rb
@@ -0,0 +1,8 @@
+class PlistEditPro < Cask
+  url 'http://www.fatcatsoftware.com/plisteditpro/PlistEditPro.zip'
+  homepage 'http://'
+  version '1.7.4'
+  sha256 '9b3da5ef5c290e7c43cdfddaae7b7cfcc7bf4ba29965f3411f30258183bf7809'
+  # no_checksum
+  link 'PlistEdit Pro.app'
+end
---
+++ Casks/sim-pholders2.rb
@@ -0,0 +1,8 @@
+class SimPholders2 < Cask
+  url 'http://simpholders.com/site/assets/files/1098/sp20a-87.zip'
+  homepage 'http://simpholders.com/'
+  version '2.0a.87'
+  sha256 '53c62d2a5817e0b7c3bb77b83dca74819ecee8e799ddc5a0832787a2456ed0a4'
+  # no_checksum
+  link 'SimPholders2.app'
+end
