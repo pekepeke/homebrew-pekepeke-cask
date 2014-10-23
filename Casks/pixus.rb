78b5a5a45eb45c172e6e93e9332f805abf67114b
pekepeke <pekepekesamurai+github@gmail.com>
2014-10-23 21:33:35 +0900

    add pixus

---
+++ Casks/pixus.rb
@@ -0,0 +1,8 @@
+class Pixus < Cask
+  url 'https://pixus.googlecode.com/files/pixus.air'
+  homepage 'https://code.google.com/p/pixus/'
+  version '1.0-r78'
+  sha256 '4c99b1a1ca7c9fe0c8d4bbae72e46070b47cd05b015ec5ca06fc98437f6594a8'
+  # no_checksum
+  link 'Pixus.app'
+end
