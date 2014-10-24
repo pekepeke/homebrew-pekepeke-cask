f5e5d9ad2639b0a2a666a3b79ca96f3ba84402b8
pekepeke <pekepekesamurai+github@gmail.com>
2014-10-24 22:27:06 +0900

    fixes sha256

--- Casks/livediag.rb
+++ Casks/livediag.rb
@@ -2,7 +2,7 @@ class Livediag < Cask
   url 'https://github.com/dataich/LiveDiag/releases/download/1.1.1/LiveDiag.app.zip'
   homepage 'http://blog.dataich.com/LiveDiag/'
   version '1.1.1'
-  sha1 '4d8fd12ff48a96888a84a0bcb94add1fca6e96fb'
+  sha256 '0ad017e3f42259968253e624c1a8c81b896d2012ba7932de774cda235a141977'
   link 'LiveDiag.app'
 end
 
