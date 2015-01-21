82d0954b8816bbe778d009ab11bc97bb471ed8fa
pekepeke <pekepekesamurai@gmail.com>
2015-01-22 08:18:36 +0900

    fixes bug.

--- Casks/permanent-eraser.rb
+++ Casks/permanent-eraser.rb
@@ -1,10 +1,10 @@
 cask :v1 => 'permanent-eraser' do
-  version 'latest'
+  version '2.6.3'
   sha256 'aa95f395054087c1498dfd352619186706ea7ad976f47facde771acc55792e88'
 
   url 'http://www.edenwaith.com/downloads/Permanent%20Eraser%202.6.3%20Beta2.zip'
   homepage 'http://www.edenwaith.com/products/permanent%20eraser/'
 
   # no_checksum
-  app 'Permanent Eraser.app'
+  app 'Permanent Eraser 2.6.3.app'
 end
