227c42da68bed13badfd748587ff8b478c06301a
pekepeke <pekepekesamurai+github@gmail.com>
2015-01-22 21:04:02 +0900

    add JFollowTail

---
+++ Casks/j-follow-tail.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'j-follow-tail' do
+  version 'latest'
+  sha256 'b7355cea9b210ee94ce634b0c365ea5c8381b57453e0b67733800a2b015e3318'
+  # no_checksum
+
+  url 'https://cj05ww.bl3302.livefilestore.com/y2m8YXj6lSOwzXID8yyrVL5m8hYDd3E7LuliugDiFWq2zlYmIg3_zFk1u8eHtAJW5S5SITpDghbSuhbjmwaapVPtwc5FmK5OTFseP005v3shIa0DnjqBD1R4wts_b_UUheziZvZJu3Z8OhdLaVyScfNng/JFollowTail.app.zip?download&psid=1'
+  homepage 'https://code.google.com/p/j-follow-tail/'
+
+  name 'JFollowTail.app'
+  app 'JFollowTail.app'
+end
