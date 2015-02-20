0b68912f7bb08aac9c61adabd676a3d2b6ab487b
pekepeke <pekepekesamurai+github@gmail.com>
2015-02-20 23:19:41 +0900

    add browse

---
+++ Casks/browse.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'browse' do
+  version 'latest'
+  sha256 'b750d495e3b458d9e200ecf6328766f87c87abbf1613cc52fbcfeb4979999754'
+  # no_checksum
+
+  url 'http://cl.ly/WL8x/Browse.zip'
+  homepage 'http://www.bzgwebs.com/'
+
+  name 'Browse.app'
+  app 'Browse.app'
+end
