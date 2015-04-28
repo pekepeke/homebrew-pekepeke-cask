855294f45b39071de8ce4304ee418597b021198a
pekepeke <pekepekesamurai+github@gmail.com>
2015-04-28 12:11:07 +0900

    update macvim version

--- Casks/macvim-kaoriya-custom.rb
+++ Casks/macvim-kaoriya-custom.rb
@@ -1,7 +1,7 @@
 cask :v1 => 'macvim-kaoriya-custom' do
 
   version 'latest'
-  sha256 '3fa97b68e0b37c762d62aba222b4da80a369d25c884962a4666455ab5432bc6d'
+  sha256 'ac474ae5be5b4d10c6b0d182507ce4c427ab82208040e399f5cdb820e58b547e'
   # no_checksum
 
   url 'https://onedrive.live.com/download.aspx?cid=7D1808F7AEC0A65A&resid=7D1808F7AEC0A65A%21126&canary=rqfVmDg24Wn2sxycveYmBjDSRExgl803fPTYU9jilgA%3D9'
